fc0428c1f38be96109eb941db4044c5da7ec1605
Velin Vangelov <vva@zylinc.com>
2022-08-06 21:06:54 +0300

    Big bang

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,24 @@
-module.exports = function(eleventyConfig) {
+// const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+// const pluginRss = require('@11ty/eleventy-plugin-rss');
+const markdownIt = require('markdown-it');
+
+module.exports = (eleventyConfig) => {
+  // eleventyConfig.addPlugin(syntaxHighlight);
+  // eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPassthroughCopy('assets/favicon.ico');
+  eleventyConfig.addPassthroughCopy('assets/fonts');
+  const options = {
+    html: true,
+    breaks: true,
+    linkify: false
+  };
+  eleventyConfig.setLibrary('md', markdownIt(options));
+
   return {
     dir: {
       input: "src",
       output: "dist"
     }
-  }
+    // htmlTemplateEngine: 'liquid'
+  };
 };
